5bca25df31fe96cf2acab99e8efa8ededc85d579
Cheng <madyayo@gmail.com>
2018-08-23 16:11:33 +0800

    add static controller

--- router.js
+++ router.js
@@ -2,12 +2,15 @@ const Router = require('koa-router');
 const router = new Router();
 const user = require('./controller/user');
 const update = require('./controller/update');
+const common = require('./controller/common');
+
 
 router.post('/user/login', user.login);
 router.get('/user/profile', user.profile);
 router.get('/update/index', update.index);
 router.get('/update/', update.index);
 router.post('/update/upload', update.upload);
+router.get('/static', common.static);
 router.get('/', ctx=>{
 	ctx.body = `建设中`
 });
